93f11308bda7a8677db241f676db767065442df4
cesalazar4 <cesalazar707@gmail.com>
2024-08-26 06:42:45 -0500

    se encontrara un pryect de Angular 08/24

---
+++ proyect2/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { PaginaComponent } from './pagina/pagina.component';
+
+export const routes: Routes = [
+    {path: '', component: PaginaComponent }
+];
